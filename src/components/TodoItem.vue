3843d01eb9a333a5623d95f1ac6a449b7c6e08e2
shovon2k19 <zaman.monir20188@gmail.com>
2019-10-10 13:22:09 +0600

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+    
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+  
+    <p><input type="checkbox" v-on:change="markComplete">{{todo.title}} 
+    
+     <button @click="$emit('todo-del',todo.id)" class="del">x</button>
+    </p>
+       
+</div>
+    
+</template>
+
+<script> 
+export default {
+    name:"TodoItem",
+    props:["todo"],
+    methods:{
+      markComplete(){
+        this.todo.completed = !this.todo.completed;
+      },
+      removeElement:function(id){
+        this.todos.$remove(id)
+      }
+    }
+    
+}
+</script>
+
+
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+   
+
+    <AddTodo v-on:add-todo="addTodo" />
+    
+ <Todos v-bind:todos="todos" v-on:todo-del ="deleteTodo" />
+
+  </div>
+</template>
+
+<script>
+import Todos from '../components/Todos'
+
+import AddTodo from '../components/AddTodo'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+   Todos,
+  
+   AddTodo
+  },
+  data(){
+    return{
+      todos:[
+   //get data from axios link
+
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err=> console.log(err));
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      const{title, completed} = newTodo;
+      axios.psot('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+      .then(res=>this.todos = [...this.todos, res.data])
+  .catch(err=>console.log(err));
+      
+    }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
+  }
+}
+</script>
+<style scoped>
+
+ form {
+    display: flex;
+  }
+
+ input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+   .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
+
